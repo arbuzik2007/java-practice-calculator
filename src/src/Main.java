36b814bbd32f425683230360ac4de5e063776e58
Orino <alenakoroleva2007@mail.ru>
2024-01-24 11:39:08 +0700

    applied function inside Main

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //"a <операция> b",
         String fileName = "input.txt";
         String[] input;
         input = FileSystem.ReadFile(fileName);
-        assert input != null;
+        if(input == null)
+        {
+            Scanner sc = new Scanner(System.in);
+            input = sc.nextLine().split(" ");
+        }
         double res = Calculator.Calculate(input);
         System.out.print(res != -1 ? res : "");
+        if(res != -1)
+            FileSystem.WriteFile(Double.toString(res),"output.txt");
     }
 }
